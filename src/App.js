303cf490fcb030c0be4c5b6a32b814f6376ad143
Kostya Sabada <kostyasabada@gmail.com>
2020-09-22 22:11:42 +0300

    ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useState } from 'react';
+import { MakeTodo } from './components/MakeTodo/index';
+import { TodoList } from './components/TodoList/TodoList';
+import { TodosFilter } from './components/TodosFilter';
+
+const FILTERS = {
+  all: 'all',
+  active: 'active',
+  completed: 'completed',
+};
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [toggleAll, setToggleAll] = useState(false);
+  const [filter, setFilter] = useState(FILTERS.all);
+
+  useEffect(() => {
+    if (localStorage.todoList) {
+      setTodoList(JSON.parse(localStorage.getItem('todoList')));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  const filteredTodos = useMemo(() => todoList.filter((todo) => {
+    switch (filter) {
+      case FILTERS.active:
+        return todo.completed === false;
+      case FILTERS.completed:
+        return todo.completed === true;
+      default:
+        return todo;
+    }
+  }), [filter, todoList]);
+
+  const changeToggleAll = () => {
+    setTodoList(
+      todoList.map(todo => ({
+        ...todo,
+        completed: !toggleAll,
+      })),
+    );
+  };
+
+  const notCompleted = todoList.filter(item => !item.completed);
+
+  useEffect(() => {
+    if (notCompleted.length === 0) {
+      setToggleAll(true);
+    } else {
+      setToggleAll(false);
+    }
+  }, [notCompleted]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+
+        <MakeTodo setTodoList={setTodoList} />
       </header>
-    </div>
+
+      {!!todoList.length && (
+        <>
+          <section className="main">
+            <input
+              type="checkbox"
+              id="toggle-all"
+              className="toggle-all"
+              checked={toggleAll}
+              onChange={changeToggleAll}
+            />
+            <label htmlFor="toggle-all">Mark all as complete</label>
+
+            <TodoList
+              todoList={todoList}
+              setTodoList={setTodoList}
+              filteredTodos={filteredTodos}
+            />
+          </section>
+
+          <footer className="footer">
+            <span className="todo-count">
+              {notCompleted.length}
+              {notCompleted.length > 1 || notCompleted.length === 0
+                ? ' todos left'
+                : ' todo left'}
+            </span>
+
+            <TodosFilter
+              setFilter={setFilter}
+              FILTERS={FILTERS}
+              filter={filter}
+            />
+
+            {notCompleted.length !== todoList.length && (
+              <button
+                type="button"
+                className="clear-completed"
+                onClick={() => setTodoList(notCompleted)}
+              >
+                Clear completed
+              </button>
+            )}
+          </footer>
+        </>
+      )}
+    </section>
   );
 }
 
